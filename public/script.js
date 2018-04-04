c8c1648bd9f94e0e7432a879357b2ae12d25259c
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-04-03 20:22:36 -0700

    Display time message was sent and received

--- public/script.js
+++ public/script.js
@@ -13,11 +13,11 @@ var Chat = {
         });
                 
         socket.on('newUser', (data) => {
-            this.appendMessage( data.sender, data.message);
+            this.appendMessage( data.sender, data.message, data.time);
         });
         
         socket.on('userJoinedRoom', (data) => {
-            this.appendMessage( data.sender, data.message);
+            this.appendMessage( data.sender, data.message, data.time);
         });        
 
         this.chatFormEl = document.getElementById('chat-form');
@@ -29,10 +29,20 @@ var Chat = {
         this.listenForMessages();
     },
 
-    appendMessage: function(sender = '', message = '') {
+    appendMessage: function(sender = '', message = '', time) {
+
+        
+        var d = new Date(time);
+
+        var hours = d.getHours();
+        var minutes = d.getMinutes();
+        console.log(`${hours}:${minutes}`);
+        
+
         var liEl = document.createElement('li');
-        liEl.innerHTML = `${sender}: ${message}`;
+        liEl.innerHTML = `${sender} ${hours}:${minutes} ${message}`;
         this.chatMessages.appendChild(liEl);
+
     },
 
     createMessage: function() {
@@ -43,22 +53,25 @@ var Chat = {
             e.preventDefault();
             
             self.sendBtn.setAttribute('disabled', 'true');
+
             socket.emit('createMessage', {
                 sender: 'User',
                 message: self.chatInputEl.value
             }, function(status) {
                 console.log('Sent.', status);
+                
+                var timeSent = new Date().getTime();
 
                 self.sendBtn.removeAttribute('disabled');
-                self.appendMessage('User', self.chatInputEl.value);
+                self.appendMessage('User', self.chatInputEl.value, timeSent);
                 self.chatInputEl.value = '';
             });            
         });
     },
 
     listenForMessages: function() {
-        socket.on('newMessage', (data) => {
-            this.appendMessage(data.sender, data.message);
+        socket.on('newMessage', (data) => {            
+            this.appendMessage(data.sender, data.message, data.time);
         });
     }
 }
