d47d5fa521dc6932c8f55c30f280c7befd299765
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-04-08 14:21:51 -0700

    Remove get request for login route

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,8 +2,4 @@ module.exports = function(app) {
     app.get('/', (req, res) => {
         res.render('index');
     });
-    
-    app.get('/login', (req, res) => {
-        res.end('login');
-    });
 }
