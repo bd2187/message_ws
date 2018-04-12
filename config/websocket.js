4512ace55cab83d057db49162086858a67b6af21
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-04-11 21:47:49 -0700

    Broadcast calls to room

--- config/websocket.js
+++ config/websocket.js
@@ -3,24 +3,35 @@ const generateMessage = require('../utils/messaging.js');
 module.exports = function(socket) {
     console.log('User connected to app'); 
     
-    // Greets individual user
-    socket.emit('newUser', generateMessage('Admin', 'Welcome'));
-
-    // Broadcast call
-    socket.broadcast.emit('userJoinedRoom', generateMessage('Session', 'New User joined chat room'));
-
-    socket.on('createMessage', (data, callbackFn) => {
-        socket.broadcast.emit('newMessage', generateMessage(data.sender, data.message));
-        callbackFn('Success from server.');
-    });
+    // socket.on('createMessage', (data, callbackFn) => {
+    //     socket.broadcast.emit('newMessage', generateMessage(data.sender, data.message));
+    //     callbackFn('Success from server.');
+    // });
 
     // Join Room
     socket.on('join', (userAndRoom, callbackFn) => {
-        console.log(userAndRoom);
+
+        const { user, room } = userAndRoom;
+        
+        socket.join(room);
+
+        // Greets individual user
+        socket.emit('newUser', generateMessage('Admin', `Welcome, you're now in ${room}`));
+
+        // Broadcast call to specified room
+        socket.broadcast.to(room).emit('userJoinedRoom', generateMessage('Session', `${user} joined ${room}.`));        
+
+        
+        socket.on('createMessage', (data, callbackFn) => {
+            socket.broadcast.to(room).emit('newMessage', generateMessage(data.sender, data.message));
+            callbackFn('Success from server.');
+        });
+        
         callbackFn();
     });
 
     socket.on('disconnect', (socket) => {
         console.log('User disconnected from app');
     });    
+
 }
